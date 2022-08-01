e453dfb4075121b11c0a6b61879de5623282c34b
Anh Duc Nguyen <108985445+anhducnguyen2006@users.noreply.github.com>
2022-08-01 16:46:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# Funkcja print()
+print("Hello World")
+
+# Typy Danych
+liczba_całkowita = 1 # int
+liczba_zmiennoprzecinkowa = 50.7 # float
+napis = "Hello World" # string (str)
+typ_logiczny = True #bool
+
+print(liczba_całkowita) # wydrukuje się 1
+print(liczba_zmiennoprzecinkowa) #wydrukuje się 50.7
+print(napis) #wydrukuje się "Hello World"
+print(typ_logiczny) #wydrukuje się True
+
+print(type(liczba_całkowita)) #wydrukuje się <class 'int'>
+print(type(liczba_zmiennoprzecinkowa)) #wydrukuje się <class 'float'>
+print(type(napis)) #wydrukuje się <class 'int'>
+print(type(typ_logiczny)) #wydrukuje się <class 'bool'>
+
+
+# =============================================================================
+# DODATKOWA INFORMACJA
+# =============================================================================
+
+# =============================================================================
+# 
+# pojedynczy komentarz może być również stworzony przez naciśnięcie klawiszów; 
+# -> na macOS: cmd + 1
+# -> na Windows: ctrl + 1
+# 
+# gdybyś chciał(a) utworzyć długi komentarz, klikasz:
+# -> na macOS: cmd + 4
+# -> na Windows: ctrl + 4
+# 
+# żeby usunąć ten komentarz, zaznaczasz tą linię kodu lub linie kodu i wciskasz 
+# klawisz 
+# -> na macOS: cmd + 5
+# -> na Windows: ctrl + 5
+# =============================================================================

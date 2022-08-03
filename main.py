2f703bdeae750f32dc31d946db279a1c478873b9
Anh Duc Nguyen <108985445+anhducnguyen2006@users.noreply.github.com>
2022-08-03 16:21:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,71 @@
-# Funkcja print()
-print("Hello World")
-
-# Typy Danych
-liczba_całkowita = 1 # int
-liczba_zmiennoprzecinkowa = 50.7 # float
-napis = "Hello World" # string (str)
-typ_logiczny = True #bool
-
-print(liczba_całkowita) # wydrukuje się 1
-print(liczba_zmiennoprzecinkowa) #wydrukuje się 50.7
-print(napis) #wydrukuje się "Hello World"
-print(typ_logiczny) #wydrukuje się True
-
-print(type(liczba_całkowita)) #wydrukuje się <class 'int'>
-print(type(liczba_zmiennoprzecinkowa)) #wydrukuje się <class 'float'>
-print(type(napis)) #wydrukuje się <class 'int'>
-print(type(typ_logiczny)) #wydrukuje się <class 'bool'>
-
+# =============================================================================
+# mnożenie napisów (str)
+# =============================================================================
+a = "Hello"
+b = 3
+print(a*b)  #HelloHelloHello
+print(a+a+a) #HelloHelloHello
 
 # =============================================================================
-# DODATKOWA INFORMACJA
+# operatory matematyczne
 # =============================================================================
+c = 3
+d = 2
+print(c+d) #drukuje się 5
+print(c-d) #drukuje się 1
+print(c*d) #drukuje się 6
+print(c/d) #drukuje się 1.5
+print(51//2) #drukuje się 25
+print(5**2) #drukuje się 25
+print(7%3) #drukuje się 1
 
 # =============================================================================
-# 
-# pojedynczy komentarz może być również stworzony przez naciśnięcie klawiszów; 
-# -> na macOS: cmd + 1
-# -> na Windows: ctrl + 1
-# 
-# gdybyś chciał(a) utworzyć długi komentarz, klikasz:
-# -> na macOS: cmd + 4
-# -> na Windows: ctrl + 4
-# 
-# żeby usunąć ten komentarz, zaznaczasz tą linię kodu lub linie kodu i wciskasz 
-# klawisz 
-# -> na macOS: cmd + 5
-# -> na Windows: ctrl + 5
+# kolejność czytania kodu, zmiana zmiennej
 # =============================================================================
+print("przed zmianą")
+x = 1
+print(x) #drukuje się 1
+print("po zmianie")
+x += 1 # 1+1
+print(x) #drukuje się 2
+print("Po odejmowaniu")
+x -= 3 
+print(x) #drukuje się -1
+
+# =============================================================================
+# jak wyjąć/wyrwać część napisu?
+# =============================================================================
+n = "HelloWorld"
+#    0123456789
+#-10-9-8-7-6-5-4-3-2-1
+print(n[0:5]) #od 0 do 5 (nie wliczając 5), drukuje się: Hello
+print(n[:5]) #drukuje się: Hello
+print(n[-2]) #drukuje się: l
+print(n[-5:-2]) #drukuje się: Wor
+print(n[-9:-3]) #drukuje się: elloWo
+print(n[2:]) #drukuje się: lloWorld
+
+# =============================================================================
+# Wbudowane funkcje napisów (str)
+# =============================================================================
+m = "Hello World. Nazywam się Andrzej"
+print(m.upper()) #drukuje się napis, gdzie wszystkie litery są duże: HELLO WORLD. NAZYWAM SIĘ ANDRZEJ
+print(m.lower()) #drukuje się napis, gdzie wszystkie litery są małe: hello world. nazywam się andrzej
+print(m.strip()) #funkcja kasuje niepotrzebne spacje z przodu i na końcu napisu
+print(len(m)) #drukuje długość napisu: 32
+print(m.replace("H","J")) #druguje napis ze zmianą liter: Jello World. Nazywam się Andrzej
+print(m.split()) #drukują się pojedyncze słowa rozdzielone spacją
+#['Hello', 'World.', 'Nazywam', 'się', 'Andrzej']
+
+# =============================================================================
+# ZADANIE
+# =============================================================================
+
+# 1) Zmień "Lubię Dizzę" na "LUBIĘ PIZZĘ"
+a = "LubięDizzę"
+b = a.upper() #LUBIĘ DIZZĘ"
+print(b.replace("D", "P"))
+print(a.upper().replace("D","P")) #funkcje można powołać po kropce, nie musi być pojedynczo 
+
+# 2) wydrukuj: bięDiz
+print(a[2:8])
